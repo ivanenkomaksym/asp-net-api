4017e9556d37249596cfeec45278bb6530e9b457
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-09-13 18:08:46 +0300

    Add ETag support for ProductsController.

---
+++ AspNetApi/Filters/ETagFilter.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using AspNetApi.Services;
+
+namespace AspNetApi.Filters
+{
+    // prevents the action filter methods to be invoked twice
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
+    public class ETagFilter : ActionFilterAttribute, IAsyncActionFilter
+    {
+        public override async Task OnActionExecutionAsync(
+            ActionExecutingContext executingContext,
+            ActionExecutionDelegate next
+        )
+        {
+            var request = executingContext.HttpContext.Request;
+
+            var executedContext = await next();
+            var response = executedContext.HttpContext.Response;
+
+            // Computing ETags for Response Caching on GET requests
+            if (
+                request.Method == HttpMethod.Get.Method
+                && response.StatusCode == (int)HttpStatusCode.OK
+            )
+            {
+                ValidateETagForResponseCaching(executedContext);
+            }
+        }
+
+        private void ValidateETagForResponseCaching(ActionExecutedContext executedContext)
+        {
+            if (executedContext.Result == null)
+            {
+                return;
+            }
+
+            var request = executedContext.HttpContext.Request;
+            var response = executedContext.HttpContext.Response;
+
+            var result = (executedContext.Result as ObjectResult).Value;
+
+            // generate ETag from LastModified property
+            //var etag = GenerateEtagFromLastModified(result.LastModified);
+
+            // generates ETag from the entire response Content
+            var etag = ETagService.ComputeWithHashFunction(result);
+
+            if (request.Headers.ContainsKey(HeaderNames.IfNoneMatch))
+            {
+                // fetch etag from the incoming request header
+                var incomingEtag = request.Headers[HeaderNames.IfNoneMatch].ToString();
+
+                // if both the etags are equal
+                // raise a 304 Not Modified Response
+                if (incomingEtag.Equals(etag))
+                {
+                    executedContext.Result = new StatusCodeResult((int)HttpStatusCode.NotModified);
+                }
+            }
+
+            // add ETag response header
+            response.Headers.Add(HeaderNames.ETag, new[] { etag });
+        }
+    }
+}
---
+++ AspNetApi/Services/ETagService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text.Json;
+using System.Text;
+
+namespace AspNetApi.Services
+{
+    public static class ETagService
+    {
+        private const string salt = "Qco52Dtp9SBbq3DkBYmhWVYgy64YIMtq";
+
+        public static string ComputeWithHashFunction(object value)
+        {
+            var serialized = JsonSerializer.Serialize(value);
+            var valueBytes = KeyDerivation.Pbkdf2(
+                             password: serialized,
+                             salt: Encoding.UTF8.GetBytes(salt),
+                             prf: KeyDerivationPrf.HMACSHA512,
+                             iterationCount: 10000,
+                             numBytesRequested: 256 / 8);
+            return Convert.ToBase64String(valueBytes);
+        }
+    }
+}
