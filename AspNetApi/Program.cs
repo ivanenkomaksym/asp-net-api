6088df408660d738e9bd9ac77a3e1401f9c3e4b6
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-09-23 12:30:27 +0300

    UseHttpLogging and output relevant request headers.

--- AspNetApi/Program.cs
+++ AspNetApi/Program.cs
@@ -28,6 +28,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseHttpLogging();
+
 app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
@@ -61,6 +63,20 @@ app.UseEndpoints(endpoints =>
     endpoints.MapGet("/", async context =>
     {
         await context.Response.WriteAsync("Hello World!");
+
+        //Output the relevant properties as the framework sees it
+        await context.Response.WriteAsync($"---As the application sees it{Environment.NewLine}");
+        await context.Response.WriteAsync($"HttpContext.Connection.RemoteIpAddress : {context.Connection.RemoteIpAddress}{Environment.NewLine}");
+        await context.Response.WriteAsync($"HttpContext.Connection.RemoteIpPort : {context.Connection.RemotePort}{Environment.NewLine}");
+        await context.Response.WriteAsync($"HttpContext.Request.Scheme : {context.Request.Scheme}{Environment.NewLine}");
+        await context.Response.WriteAsync($"HttpContext.Request.Host : {context.Request.Host}{Environment.NewLine}");
+
+        //Output relevant request headers (starting with an X)
+        await context.Response.WriteAsync($"{Environment.NewLine}---Request Headers starting with X{Environment.NewLine}");
+        foreach (var header in context.Request.Headers.Where(h => h.Key.StartsWith("X", StringComparison.OrdinalIgnoreCase)))
+        {
+            await context.Response.WriteAsync($"Request-Header {header.Key}: {header.Value}{Environment.NewLine}");
+        }
     });
 });
 
