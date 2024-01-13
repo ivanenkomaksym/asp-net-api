1bfc028e6e234ed1d7b4900d1451ef70027fbfb8
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-01-13 15:07:37 +0200

    Add basic integration tests.

---
+++ AspNetApi.IntegrationTests/ApplicationBase.cs
@@ -0,0 +1,25 @@
+﻿using AspNetApi.Controllers;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit.Abstractions;
+
+namespace AspNetApi.IntegrationTests
+{
+    internal class ApplicationBase : WebApplicationFactory<Program>
+    {
+        private readonly ITestOutputHelper Output;
+        public ApplicationBase(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddControllers().AddApplicationPart(typeof(WeatherForecastController).Assembly);
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
---
+++ AspNetApi.IntegrationTests/BasicTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AspNetApi.IntegrationTests
+{
+    public sealed class BasicTests
+    {
+        private readonly ITestOutputHelper Output;
+
+        public BasicTests(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
+        [Fact]
+        public async Task GetWeatherForecast()
+        {
+            // Arrange
+            using var application = new ApplicationBase(Output);
+            var client = application.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/WeatherForecast");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+        }
+    }
+}
