86e5320f4c5c116fb1b1640adb028027e6d7fafb
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-01-24 11:32:40 +0200

    Add CanSerializeInterface test.

--- AspNetApi.IntegrationTests/JsonConverterTests.cs
+++ AspNetApi.IntegrationTests/JsonConverterTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -23,6 +24,13 @@ namespace AspNetApi.IntegrationTests
         public IMyBase MyBase { get; set; }
     }
 
+    [JsonDerivedType(typeof(Derived1), typeDiscriminator: "Derived1")]
+    [JsonDerivedType(typeof(Derived2), typeDiscriminator: "Derived2")]
+    public record BaseType(int Id);
+
+    public record Derived1(int Id, string Name) : BaseType(Id);
+    public record Derived2(int Id, bool IsActive) : BaseType(Id);
+
     public sealed class JsonConverterTests
     {
         private readonly ITestOutputHelper Output;
@@ -44,6 +52,30 @@ namespace AspNetApi.IntegrationTests
             string json = JsonSerializer.Serialize(values);
             var exception = Assert.Throws<System.NotSupportedException>(() => JsonSerializer.Deserialize<List<Implementation>>(json));
             Assert.Contains("Deserialization of interface types is not supported.", exception.Message);
+        }
+
+        [Fact]
+        public void CanSerializeInterface()
+        {
+            var expected = new List<BaseType>
+            {
+                new Derived1(123, "Foo"),
+                new Derived2(456, true)
+            };
+            string json = JsonSerializer.Serialize(expected);
+            var actual = JsonSerializer.Deserialize<List<BaseType>>(json);
+
+            Assert.Equal(expected.Count, actual.Count);
+
+            var expectedDerived1 = Assert.IsType<Derived1>(expected[0]);
+            var actualDerived1 = Assert.IsType<Derived1>(actual[0]);
+
+            Assert.Equal(expectedDerived1.Name, actualDerived1.Name);
+
+            var expectedDerived2 = Assert.IsType<Derived2>(expected[1]);
+            var actualDerived2 = Assert.IsType<Derived2>(actual[1]);
+
+            Assert.Equal(expectedDerived2.IsActive, actualDerived2.IsActive);
         }
     }
 }
